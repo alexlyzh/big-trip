3f8ea555f40b4c7a2a78d67e2422ad37299e8783
Alex <al.lyzhin@gmail.com>
2021-11-05 21:09:31 +0300

    Исплавлен service worker для gh-pages

--- src/view/points-list-view.js
+++
@@ -1,8 +0,0 @@
-import Abstract from './abstract.js';
-
-export default class PointsListView extends Abstract {
-  getTemplate() {
-    return `<ul class="trip-events__list">
-            </ul>`;
-  }
-}
